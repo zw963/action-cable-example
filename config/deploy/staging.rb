620188721f80e3c7ad2de985f0c412c890b77c66
Billy.Zheng <zw963@163.com>
2016-12-15 20:35:44 +0800

    Add js runtime.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,8 @@
-set :branch, 'dev'
+# set :branch, 'dev'
 role :web, %w{action-cable-example@www.ershou_web.test.xthinkapp.com}
 role :app, %w{action-cable-example@www.ershou_web.test.xthinkapp.com}
 role :db,  %w{action-cable-example@www.ershou_web.test.xthinkapp.com}
+set :rvm_ruby_version, "ruby-2.2.4@#{fetch(:application)}"
 
 #  set :ssh_options, {
 #    keys: %w(/home/zw963/.ssh/id_rsa),
