059a856398ef4c48e5aa081c9e538bb628965c85
Billy.Zheng <zw963@163.com>
2016-12-15 17:05:31 +0800

    Update staging config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,6 @@
 set :application, 'action-cable-example'
 set :deploy_to, -> { "/var/www/#{fetch(:application)}/#{fetch(:application)}_#{fetch(:stage)}" }
 set :branch, -> { `git rev-parse --abbrev-ref HEAD`.chomp }
-# lock '3.7.0'
 
 # 本地 git 示例: ssh://git@example.com:30000/~/me/my_repo.git
 # svn 示例: svn://myhost/myrepo/#{fetch(:branch)}
@@ -9,19 +8,22 @@ set :repo_url, 'git@github.com:zw963/action-cable-example.git'
 set :keep_releases, 5
 set :tmp_dir, "#{fetch(:deploy_to)}/tmp"
 
-
 set :local_user, -> { Etc.getlogin }
+set :rails_env, -> { fetch(:stage) }
 
 set :linked_files, %w{
   config/database.yml
   config/secrets.yml
 }
-set :linked_files, %w{
+set :linked_dirs, %w{
   log
   tmp
-  vendor/bundle
-  public/system
-  public/uploads
 }
 
+  # vendor/bundle
+  # public/system
+  # public/uploads
+
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :rvm_custom_path, '~/.myveryownrvm'
+# lock '3.7.0'
